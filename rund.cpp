786d1772d9d9e2be1c0959d7797bfa728b9fefce
Misha <bl365@bk.ru>
2016-04-06 16:34:27 +0300

    3

--- rund.cpp
+++ rund.cpp
@@ -1,62 +1,61 @@
-#include <cstdlib>
+#include <stdlib.h>
 #include <iostream>
 #include <ctime>
 using namespace std;
 
-	class matr
+class matr
+{
+	int i, j;
+	int **arr;
+	public:
+	matr(int numbersLine, int numbersColumn)
+	{
+		i = numbersLine;
+		j = numbersColumn;
+		arr = new int* [i];
+		for (int a = 0; a < i; a++)
 		{
-				int i,j,c,d;
+			arr [a] = new int [j];
+		}
+	}
 
-				public:
-				int **mass;
-				matr(int I,int J)
-					{
-						i = I;
-						j = J;
-						int **mass = new int* [i];
-						for (c = 0; c < i; c++)
-							{
-								mass[c] = new int [j];
-							}
-					}
+	int method_rand();
+	void get_matr ();
 
-				int method_rand();
-				void get_matr ();
+	~matr()
+	{
+		delete [] arr;
+	}
+};
 
-				~matr()
-					{
-						delete [] mass;
-					}
-		};
-
-	void matr::get_matr ()
-        {
-        	for (c = 0; c < i; c++)
-        		{
-        			for (d = 0; d < j; d++)
-        			{
-        				cout<< mass [c] [d]<<endl;
-        			}
-        		}
-        }
-	int matr::method_rand()
+void matr::get_matr ()
+{
+	for (int a = 0; a < i; a++)
+	{
+		for (int b = 0; b < j; b++)
 		{
-				srand(time(NULL));
-				for (int z = 0; z < i; z++)
-				{
-					for (int y = 0; y < j; y++)
-					{
-						mass [z] [y] = rand ();
-					}
-				}
-				return 0;
-		};
-
-	int main()
+				cout << arr [a] [b] << endl;
+		}
+	}
+}
+int matr::method_rand()
+{
+	srand(time(NULL));
+	for (int a = 0; a < i; a++)
+	{
+		for (int b = 0; b < j; b++)
 		{
-				matr Obj(2, 3);
-				Obj.method_rand();
-				Obj.get_matr();
-				return 0;
-		};
+			arr [a] [b] = rand ();
+		}
+	}
+	return 0;
+};
+
+int main()
+{
+matr Obj(2, 3);
+Obj.method_rand();
+Obj.get_matr();
+return 0;
+}
 
